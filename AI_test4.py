d08740551325c802eb1e466152b91584caed7fc4
= <publicuser0999847695359@gmail.com>
2025-10-11 10:12:56 -0400

    remove unecessary duplicates

--- AI_test4.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer("all-mpnet-base-v2")
-context = "The strangeness of entering as ___ the house where she had so long commanded."
-choices = ["an investor", "a suppliant", "an authority", "a fanatic"]
-
-context_emb = model.encode(context)
-scores = [util.cos_sim(model.encode(context.replace("___", c)), context_emb) for c in choices]
-print(scores)
